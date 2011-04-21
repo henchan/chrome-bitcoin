5d9add222f61ab28c21c82a274ff0a7153d57527
Kiba <hackerkiba@gmail.com>
2011-04-21 16:51:20 +0000

    success

--- account_view.js
+++ account_view.js
@@ -2,4 +2,8 @@
 function listaccounts()
 {
   var params = '{"jsonrpc": "1.0", "id":"chrome", "method": "listaccounts", "params": [] }';
+  var success = function (res)
+  {
+    var info = JSON.parse(res);
+  }
 }
